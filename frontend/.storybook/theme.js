5ea743c273b56caa96db1ed3368f2b6243c31ed0
eyeinthesky <6edroid@gmail.com>
2025-05-01 16:08:55 +0530

    chore: remove Storybook config directory

--- frontend/.storybook/theme.js
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming';
-
-export default create({
-  base: 'light',
-  brandTitle: 'VibeKit',
-  brandUrl: 'https://github.com/eyeinthesky6/VibeKit',
-  brandImage: null,
-});
